8a5ec67dfc6223525a9baad8ac92fbe9d05b0afa
whx <whx@lumokeji.com>
2018-11-02 14:50:41 +0800

    删除文件

--- admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//单一入口
-	//链接形式：localhost/index.php?（admin.php?）controller=控制器名&method=方法名
-	header("Content-type: text/html; charset=utf-8");
-	require_once('framework/Common/MVC.php');
-	MVC::run($config,$validate);
-?>
